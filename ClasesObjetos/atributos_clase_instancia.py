56b25e24f3727578c123df9ee1d67ea2e34baf60
frodinsky <rodolara97@gmail.com>
2024-11-21 15:57:40 -0700

    Modificacion de algunos comentarios

--- ClasesObjetos/atributos_clase_instancia.py
+++ ClasesObjetos/atributos_clase_instancia.py
@@ -1,16 +1,18 @@
 class Persona:
-#
     atributo_clase = 0
 
-    def __init__(self,atributo_instancia):
+    def __init__(self, atributo_instancia):
         self.atributo_instancia = atributo_instancia
+
 
 #programa principal
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print("***Atributo de clase***")
     print(f"Atributo de instancia {Persona.atributo_clase}")
+
     #Modificamos el atributo de clase
+
     Persona.atributo_clase = 10
     print(f"Modificacion {Persona.atributo_clase}")
 
@@ -25,4 +27,3 @@ if __name__=="__main__":
     print("\t-------")
     print(f"Clase {persona2.atributo_clase}")
     print(f"Instancia {persona2.atributo_instancia}")
-
