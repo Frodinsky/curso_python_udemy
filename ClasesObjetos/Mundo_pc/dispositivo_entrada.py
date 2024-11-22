b448ccf3917dcf81dbfe7504ce4a64982407622b
frodinsky <rodolara97@gmail.com>
2024-11-22 00:37:30 -0700

    Sin novedades

--- ClasesObjetos/Mundo_pc/dispositivo_entrada.py
+++ ClasesObjetos/Mundo_pc/dispositivo_entrada.py
@@ -1,5 +1,5 @@
 class DispositivoEntrada:
+    def __init__(self, marca, tipo_entrada):
+        self.marca = marca
+        self.tipo_entrada = tipo_entrada
 
-     def __init__(self,marca,tipo_entrada):
-         self._marca = marca
-         self._tipo_entrada = tipo_entrada
